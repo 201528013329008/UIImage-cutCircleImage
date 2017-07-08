1b1a545ff9f621653014dd47b69b880e0a51184e
jingyi <1849612320@qq.com>
2017-07-08 18:21:09 +0800

    Initial Commit

---
+++ 圆角/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  圆角
+//
+//  Created by 691 on 2017/7/8.
+//  Copyright © 2017年 691. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 圆角/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  圆角
+//
+//  Created by 691 on 2017/7/8.
+//  Copyright © 2017年 691. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
